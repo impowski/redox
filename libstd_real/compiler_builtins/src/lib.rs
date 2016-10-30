68f8a0a91ade8a6425908a6363de5a58440ba89a
Jeremy Soller <jackpot51@gmail.com>
2016-10-30 16:15:10 -0600

    Implement compiler builtins and update rust

--- libstd_real/compiler_builtins/src/lib.rs
+++ libstd_real/compiler_builtins/src/lib.rs
@@ -1,3 +1,5 @@
+#![no_std]
+
 /// Memcpy
 ///
 /// Copy N bytes of memory from one location to another.
--- libstd_real/libc/src/lib.rs
+++ libstd_real/libc/src/lib.rs
@@ -4,15 +4,12 @@
 #![feature(naked_functions)]
 
 pub use types::*;
-pub use builtins::*;
 pub use funcs::*;
 pub use start::*;
 pub use syscall::*;
 
 /// Basic types (not usually system specific)
 mod types;
-/// Builtins like memcpy and memmove that Redox does not have
-mod builtins;
 /// Basic functions (not system specific)
 mod funcs;
 /// Start function and call in to libstd
